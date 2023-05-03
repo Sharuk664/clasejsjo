5e459920ff0cecec3c62adc6d664d789dfc3f624
Sharuk664 <sharukpomavivas@iejoseobrero.edu.pe>
2023-05-03 09:18:50 -0500

    actualizacion

---
+++ Condicionales/index.js
@@ -0,0 +1,55 @@
+/*
+//Ejercicio 1
+if ("0") {
+    alert( 'Hello' );
+}
+*/
+
+/*
+//Ejecicio 2
+let respuesta = prompt("¿Cuál es el nombre oficial de JavaScript?")
+
+if (respuesta == "ECMAScript") {
+    alert("Correcto");
+} else {
+    alert("¿No lo sabes? ¡ECMAScript!")
+}
+*/
+
+/*
+//Ejercicio 3
+let pregunta = prompt("Inserta un número")
+
+if (pregunta > 0) {
+    alert("Correcto")
+} else {
+    alert("Incorrepto, intente denuevo")
+}
+
+let trabajo = prompt("Inserta un número")
+
+if (trabajo == 0) {
+    alert("Correcto")
+} else {
+    alert("Incorrepto, intente denuevo")
+}
+
+let labor = prompt("Inserta un número")
+
+if (labor < -1) {
+    alert("Correcto")
+} else {
+    alert("Incorrepto, intente denuevo")
+}
+
+let numero = prompt("Ingrese un número")
+if (numero == "0") {
+    alert("0")
+} else if (numero > "0") {
+    alert("1")
+} else if (numero < "0") {
+    alert("-1")
+} else {
+    alert("No se registro el número")
+}
+*/
---
+++ trabajo/index.js
@@ -0,0 +1,22 @@
+/*
+let cond = prompt(year == 2015);
+if (cond) {
+    alert('tienes un valor valido');
+}
+*/
+
+/*
+ejercicio
+if ("0") {
+    alert( 'Hello' );
+}
+*/
+
+/*
+let coment = ("What's the name oficial de javascript");
+if (coment) {
+    alert("No sabes?")
+} else if (coment) {
+    
+}
+*/
