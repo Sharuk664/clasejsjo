5b8732fdc3cb92c984760efa1b113179121f424f
Sharuk664 <sharukpomavivas@iejoseobrero.edu.pe>
2023-05-08 10:10:21 -0500

    Mejora 2

---
+++ Operadores_Logicos/index.js
@@ -0,0 +1,35 @@
+//OR ||
+/*
+//EJemplo 1:
+if (1 || 0) {
+    alert("Valor verdadero¡")
+} else {
+    alert('Valor falso¡')
+}
+*/
+
+/*
+//Ejemplo 2
+let hour = 9
+if (hour < 10 || hour > 18) {
+    alert('La oficina esta cerrada.')
+}
+*/
+
+/*
+//Ejemplo 3
+let firsNane = "";
+let lastName = "";
+let NickName = "SuperCoder";
+
+alert( firsNane || lastName || NickName || "Anonymous")
+*/
+
+
+//AND &&
+/*
+alert(true && true); // true
+alert(false && true); // false
+alert(true && false); // false
+alert(false && false); // false
+*/
